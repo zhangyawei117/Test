d0b0e5bc11d6084d1143cbe6386dff85a9f3263b
zywxxd117 <zyw901123@163.com>
2018-06-28 11:08:56 +0800

    222222

---
+++ FirstDemo/src/cn/langchao/Demo2.java
@@ -0,0 +1,20 @@
+package cn.langchao;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ * 
+ *
+ * @author zhangyawei
+ * @version C10 2018Äê6ÔÂ28ÈÕ
+ * @since SDP V300R003C10
+ */
+public class Demo2
+{
+    @Test
+    public void test1()
+    {
+        System.out.println("222222222222");
+    }
+}
