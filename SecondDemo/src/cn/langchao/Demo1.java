39a5a541c4160949ea63622504565f6c40f34027
zywxxd117 <zyw901123@163.com>
2018-06-28 11:38:06 +0800

    3333

--- SecondDemo/src/cn/langchao/Demo1.java
+++ SecondDemo/src/cn/langchao/Demo1.java
@@ -2,9 +2,19 @@ package cn.langchao;
 
 import org.junit.jupiter.api.Test;
 
-public class Demo1 {
-	@Test
-	public void test() {
-		System.out.println("hhhhh");
-	}
+/**
+ * 
+ * 
+ *
+ * @author zhangyawei
+ * @version C10 2018Äê6ÔÂ28ÈÕ
+ * @since SDP V300R003C10
+ */
+public class Demo1
+{
+    @Test
+    public void test()
+    {
+        System.out.println("hhhhh");
+    }
 }
