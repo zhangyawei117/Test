6e23fdb74bdacb966f686e16d4d0048d1760f9e3
zywxxd117 <zyw901123@163.com>
2018-07-12 11:20:07 +0800

    新增的循环语句

--- FirstDemo/src/cn/langchao/Demo4.java
+++ FirstDemo/src/cn/langchao/Demo4.java
@@ -7,5 +7,8 @@ public class Demo4
     @Test
     public void testDemo() {
         System.out.println("你好，我是周杰伦！");
+        for(int i=1;i<=100;i++) {
+            System.out.println(i);
+        }
     }
 }
