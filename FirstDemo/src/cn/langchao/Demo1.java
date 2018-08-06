d7dad9a4249dbef550b10a236c8116515f0f7ddc
zywxxd117 <zyw901123@163.com>
2018-08-06 19:36:49 +0800

    贱人好可怕

--- FirstDemo/src/cn/langchao/Demo1.java
+++ FirstDemo/src/cn/langchao/Demo1.java
@@ -1,9 +1,14 @@
 package cn.langchao;
 
+import org.junit.Test;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
 		System.out.println("qqqqqqqqq");
 	}
-
+	@Test
+	public void test() {
+	    System.out.println("贱人好可怕！");
+	}
 }
